c3964656c6358ce6e989e282d0c71d83d3758f6d
ddooggoori <97645062+ddooggoori@users.noreply.github.com>
2022-02-18 11:10:35 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+test용입니다^-^
+
+import pandas as pd
+pd.read_csv(r"C:\sepsis3.csv")
+
+
